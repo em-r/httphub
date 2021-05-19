58026733d5c005a8447ef717b6150a9d9d168b15
El Mehdi Rami <lbiggboss19@gmail.com>
2021-05-20 00:45:15 +0100

    added logic to handle requests with json body

--- httphub/helpers/response.go
+++ httphub/helpers/response.go
@@ -1,6 +1,7 @@
 package helpers
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/ElMehdi19/httphub/httphub/structs"
@@ -15,6 +16,16 @@ func MakeResponse(r *http.Request) structs.HTTPMethodsResponse {
 		Args:    r.URL.Query(),
 		Headers: r.Header,
 		Origin:  r.Header.Get("origin"),
+	}
+	if r.Method == "GET" {
+		return resp
+	}
+
+	var jsonBody interface{}
+	if err := json.NewDecoder(r.Body).Decode(&jsonBody); err == nil {
+		resp.JSON = jsonBody
+	} else {
+		resp.JSON = err.Error()
 	}
 	return resp
 }
--- httphub/structs/response.go
+++ httphub/structs/response.go
@@ -4,10 +4,10 @@ package structs
 // that will be sent back to clients requesting
 // any of the http_methods endpoint.
 type HTTPMethodsResponse struct {
-	URL     string                 `json:"url"`
-	Args    map[string][]string    `json:"args,omitempty"`
-	Headers map[string][]string    `json:"headers"`
-	Origin  string                 `json:"origin,omitempty"`
-	JSON    map[string]interface{} `json:"json,omitempty"`
-	Method  string                 `json:"method,omitempty"`
+	URL     string              `json:"url"`
+	Args    map[string][]string `json:"args,omitempty"`
+	Headers map[string][]string `json:"headers"`
+	Origin  string              `json:"origin,omitempty"`
+	JSON    interface{}         `json:"json,omitempty"`
+	Method  string              `json:"method,omitempty"`
 }
