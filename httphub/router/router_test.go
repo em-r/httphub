047a7be4bea7e1775e3d72915697034529d965b4
El Mehdi Rami <lbiggboss19@gmail.com>
2021-05-20 03:31:09 +0100

    /post testing routing

--- httphub/router/router_test.go
+++ httphub/router/router_test.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/ElMehdi19/httphub/httphub/structs"
@@ -34,4 +35,23 @@ func TestMethodGet(t *testing.T) {
 		assert.FailNow(err.Error())
 	}
 	assert.True(reflect.DeepEqual(map[string][]string{"x": {"1"}}, body.Args))
+}
+
+func TestMethodPost(t *testing.T) {
+	assert := assert.New(t)
+	base, tearDown := setUpTestServer()
+	defer tearDown()
+
+	resp, err := http.Post(fmt.Sprintf("%s/post?x=1", base), "text/plain", strings.NewReader("test"))
+	assert.NoError(err)
+	assert.Equal(resp.StatusCode, http.StatusOK)
+
+	var body structs.HTTPMethodsResponse
+	if err := json.NewDecoder(resp.Body).Decode(&body); !assert.NoError(err) {
+		assert.FailNow(err.Error())
+	}
+
+	assert.True(reflect.DeepEqual(map[string][]string{"x": {"1"}}, body.Args))
+	assert.Equal("test", body.Data)
+	assert.Equal(body.Headers["Content-Type"][0], "text/plain")
 }
