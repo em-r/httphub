d2a6b945f3e558b39a4672adc6f7d95003b5c227
El Mehdi Rami <lbiggboss19@gmail.com>
2021-05-23 19:45:38 +0100

    moved request body parsing to its own function

--- httphub/helpers/response.go
+++ httphub/helpers/response.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"io"
+	"log"
 	"net/http"
 
 	"github.com/ElMehdi19/httphub/httphub/structs"
@@ -13,6 +14,27 @@ func parseJSON(r io.Reader) (interface{}, error) {
 	var jsonBody interface{}
 	err := json.NewDecoder(r).Decode(&jsonBody)
 	return jsonBody, err
+}
+
+func parseBody(r *http.Request, resp *structs.HTTPMethodsResponse) {
+	switch r.Header.Get("content-type") {
+	case "application/json":
+		if body, err := parseJSON(r.Body); err == nil {
+			resp.JSON = body
+		} else {
+			resp.JSON = err.Error()
+		}
+	case "application/x-www-form-urlencoded":
+		if err := r.ParseForm(); err == nil {
+			resp.Form = r.PostForm
+		} else {
+			log.Println(err.Error())
+		}
+	default:
+		body := bytes.NewBuffer(nil)
+		io.Copy(body, r.Body)
+		resp.Data = body.String()
+	}
 }
 
 // MakeResponse creates and returns
@@ -25,28 +47,12 @@ func MakeResponse(r *http.Request) structs.HTTPMethodsResponse {
 		Headers: r.Header,
 		Origin:  r.Header.Get("origin"),
 	}
+
 	if r.Method == "GET" {
 		return resp
 	}
 
-	switch r.Header.Get("content-type") {
-	case "application/json":
-		if body, err := parseJSON(r.Body); err == nil {
-			resp.JSON = body
-		} else {
-			resp.JSON = err.Error()
-		}
-	case "application/x-www-form-urlencoded":
-		if err := r.ParseForm(); err == nil {
-			resp.Form = r.PostForm
-		} else {
-			resp.Args = map[string][]string{"error": {err.Error()}}
-		}
-	default:
-		body := bytes.NewBuffer(nil)
-		io.Copy(body, r.Body)
-		resp.Data = body.String()
-	}
+	parseBody(r, &resp)
 
 	return resp
 }
