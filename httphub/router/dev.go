3d812a755d9b4d26087255a9c9fe2f17af4f63f1
El Mehdi Rami <lbiggboss19@gmail.com>
2021-05-30 01:02:40 +0100

    debug handler complete

--- httphub/router/dev.go
+++ httphub/router/dev.go
@@ -5,21 +5,44 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 
-	"github.com/alecthomas/chroma/quick"
+	"github.com/alecthomas/chroma/formatters/html"
+	"github.com/alecthomas/chroma/lexers"
+	"github.com/alecthomas/chroma/styles"
 )
 
+// SourceCodeHandler displays the content of the file specified in the path
+// query argument with syntax highlighting and highlights the line passed
+// in the line query argument.
 func SourceCodeHandler(w http.ResponseWriter, r *http.Request) {
 	path := r.FormValue("path")
+
 	f, err := os.Open(path)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 	defer f.Close()
-
 	buf := bytes.NewBuffer(nil)
 	io.Copy(buf, f)
-	if err := quick.Highlight(w, buf.String(), "go", "html", "github"); err != nil {
+
+	line := r.FormValue("line")
+	lineNum, err := strconv.Atoi(line)
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+	lines := [][2]int{
+		{lineNum, lineNum},
+	}
+
+	formatter := html.New(html.TabWidth(4), html.WithLineNumbers(true), html.HighlightLines(lines))
+	style := styles.Get("github")
+	lexer := lexers.Get("go")
+	iterator, err := lexer.Tokenise(nil, buf.String())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("content-type", "text/html")
+	formatter.Format(w, style, iterator)
 }
--- httphub/router/router.go
+++ httphub/router/router.go
@@ -3,13 +3,14 @@ package router
 import (
 	"net/http"
 
+	"github.com/ElMehdi19/httphub/httphub/helpers"
 	"github.com/ElMehdi19/httphub/middlewares"
 	"github.com/gorilla/mux"
 )
 
 func New() http.Handler {
 	handler := mux.NewRouter()
-	handler.Use(middlewares.Logger)
+	handler.Use(middlewares.Recover, middlewares.Logger)
 
 	handler.HandleFunc("/get", ViewGet).Methods("GET")
 	handler.HandleFunc("/put", ViewPut).Methods("PUT")
@@ -17,6 +18,10 @@ func New() http.Handler {
 	handler.HandleFunc("/patch", ViewPatch).Methods("patch")
 	handler.HandleFunc("/delete", ViewDelete).Methods("delete")
 	handler.HandleFunc("/any", ViewAny)
+
+	if helpers.IsDevMode() {
+		handler.HandleFunc("/debug", SourceCodeHandler)
+	}
 
 	return handler
 }
