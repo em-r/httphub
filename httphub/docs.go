b8930ee2290d2203f2ac4067565c109a31a25487
El Mehdi Rami <lbiggboss19@gmail.com>
2021-05-31 05:32:46 +0100

    add swagger docs

---
+++ httphub/docs.go
@@ -0,0 +1,22 @@
+// Package classification HTTPHub
+//
+// An HTTP Request/Response service fully written in Go
+//
+//     Schemes: http, https
+//     Host: httphub.io
+//     BasePath: /
+//     Version: 1.0.0
+//     License: MIT http://opensource.org/licenses/MIT
+//     Contact: El Mehdi Rami<mehdi@httphub.io> https://mehdi.codes
+//
+//     Consumes:
+//     - application/json
+//     - application/x-www-form-urlencoded
+//
+//     Produces:
+//     - application/json
+//     - text/html
+//     - text/plain
+//
+// swagger:meta
+package main
