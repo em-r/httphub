6563a2561094ebc4f0c30bd6ee095c5ebcc880b2
El Mehdi Rami <lbiggboss19@gmail.com>
2021-06-01 15:48:50 +0100

    moved CORS middleware to headers module

--- httphub/middlewares/headers.go
+++ httphub/middlewares/headers.go
@@ -10,3 +10,11 @@ func JSONContent(h http.Handler) http.Handler {
 		h.ServeHTTP(w, r)
 	})
 }
+
+// CORS sets cors-related headers in the response object.
+func CORS(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		h.ServeHTTP(w, r)
+	})
+}
