860397bf720c3a48f83c26134ff8b7d72f3cca70
El Mehdi Rami <lbiggboss19@gmail.com>
2021-06-07 23:46:23 +0100

    add more headers to CORS middleware

--- httphub/middlewares/headers.go
+++ httphub/middlewares/headers.go
@@ -14,7 +14,15 @@ func JSONContent(h http.Handler) http.Handler {
 // CORS sets cors-related headers in the response object.
 func CORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		if origin := r.Header.Get("Origin"); origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		} else {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+		}
+
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, PATCH, OPTIONS, HEAD")
 		h.ServeHTTP(w, r)
 	})
 }
